24155e7ee049cda85a404aca5d7d21ce660211b1
rosewhitt <rosewhitt@icloud.com>
2018-06-14 16:59:26 +0000

    Done.

--- app/models/moviehash.rb
+++ app/models/moviehash.rb
@@ -4,62 +4,86 @@ movies = {
 
     genre: {
 
-      romance: [],
+      romance: {
+        romcom: ['Clueless', 'Theres Something About Mary', '17 Again', 'Juno', 
+          'Lonesome Jim'],
+        romdrama: ['Rebel Without a Cause', 'Juno', 'Titanic', 'Forrest Gump', 
+          'Walking The Dead'],
+        tearjerker: ['Titanic'],
+        allromance: ['Clueless', 'Theres Something About Mary', '17 Again', 'Juno',
+           'Rebel Without a Cause', 'Juno', 'Titanic', 'Forrest Gump', 'Titanic', 
+           'Fast Times at Ridgemont High', 'Lonesome Jim', 'Walking The Dead']
+      },
 
-      romcom: [],
-
-      romdrama: [],
-
-      crime: [],
-
-      mystery: [],
-    },
 
       drama: {
-        true: [],
-        crime: [],
-        military: [],
-        historical: [],
-        mystery: [],
-        romdrama: []
+        true: ['The Blind Side'],
+        crime: ['Oceans series', 'Heathers', 'Antibodies', 'Murder in the First'],
+        military: ['Top Gun', 'City of Life and Death'],
+        historical: ['Titanic', 'City of Life and Death'],
+        mystery: ['IT', 'The Shining', 'Walking The Dead', 'Murder in the First'],
+        romdrama: ['Rebel Without a Cause', 'Juno', 'Titanic', 'Forrest Gump', 
+          'Walking The Dead'],
+        alldrama: ['Lady Bird', 'The Blind Side', 'Oceans series', 'Heathers',
+           'Top Gun', 'Titanic', 'IT', 'The Shining', 'Rebel Without a Cause', 
+           'Juno', 'Titanic', 'Forrest Gump', 'City of Life and Death', 
+           'Antibodies', 'Manic', 'The Beautiful Country', 'Walking The Dead', 
+           'Murder in the First', 'Driving Miss Daisy', 'Pleasantville']
       },
 
       horror: {
         comedyhor: [],
-        psychhor: [],
-        supernaturalhor: [],
-        realhorror: [],
-        cyberhorror: []
+        psychhor: ['The Shining', 'Split'],
+        supernaturalhorror: [],
+        realhorror: ['Get Out'],
+        cyberhorror: [],
+        actionhorror: ['IT'],
+        slasher: ['Halloween'],
+        allhorror: ['The Shining', 'Split', 'Get Out', 'IT', 'Halloween', 'Horror Express']
       },
 
-      fantasy: {
-        dystopian: [],
-        superhero: [],
-        magic: [],
-        alien: [],
-        future: []
-      },
+      fantasy: ['Spaceballs', 'Pleasantville'],
+        
+      
 
-      documentary: {
-        war: [],
-        sports: []
-      },
+      documentary: ['Blackfish', 'Man on Wire', '13th'],
+
       comedy: {
-        romcom: [],
-        slapstick: []
+        romcom: ['Clueless', 'Theres Something About Mary', '17 Again', 'Juno', 
+          'Lonesome Jim'],
+        allcomedy: ['Ferris Buellers Day Off', 'Clueless', 'Oceans series', 
+          'Deadpool series', 'Teenage Mutant Ninja Turtles series',
+           'Theres Something About Mary', 'Heathers', 'Clueless', '17 Again', 
+           'National Lampoons series', 'Monty Python series', 
+           'Caddyshack', 'Spaceballs', 'Dumb and Dumber series', 'Happy Gilmore', 
+           'Tommy Boy', 'Ace Ventura: Pet Detective', 'Hot Shots!', 'The Waterboy', 
+           'Lady Bird', 'Jawbreaker', 'Fast Times at Ridgemont High', 
+           'The Winning Season', 'Juno', 'Lonesome Jim', 'Pleasantville']
       },
 
       action: {
         spy: [],
-        govwar: [],
+        govwar: ['Hot Shots!', 'Top Gun'],
         nature: [],
-        crime: []
+        crime: ['Oceans series', 'Ace Ventura: Pet Detective', 'Red Sun'],
+        superhero: ['Spider-Man series', 'Wonder Woman', 'Deadpool series',
+           'Teenage Mutant Ninja Turtles series'],
+        allaction: ['IT', 'Ace Ventura: Pet Detective', 'Hot Shots!', 
+          'Top Gun', 'Oceans series', 'Ace Ventura: Pet Detective', 
+          'Spider-Man series', 'Wonder Woman', 'Deadpool series', 
+          'Teenage Mutant Ninja Turtles series', 'Red Sun']
       },
 
-      throwback: {
-        classics: [],
-        other: []
-      }
+      throwback: ['Ferris Buellers Day Off', 'Clueless', 'The Shining',
+         'Halloween', 'National Lampoons series', 'Monty Python series', 
+         'Caddyshack', 'Spaceballs', 'Dumb and Dumber series', 'Tommy Boy', 
+         'Happy Gilmore', 'Hot Shots!', 'The Waterboy', 'Rebel Without a Cause', 
+         '17 Again', 'Titanic', 'Forrest Gump', 'Jawbreaker', 'Driving Miss Daisy'],
+
+      sport: ['The Blind Side', 'Caddyshack', 'Happy Gilmore', 'The Waterboy', 
+        'The Winning Season'],
+
+      sad: ['The Blind Side']
 
     },
 
@@ -71,25 +95,32 @@ movies = {
 
       genre: {
 
-        action: ['Bambi'],
+        action: ['Bambi', 'Up', 'Finding Nemo series', 'Zootopia', 'WALL-E',
+           'Coraline', 'Frozen', 'Moana', 'E.T. The Extra Terrestrial',
+           'The Muppet Movie', 'Despicable Me series', 'Inside Out'],
 
-        comedy: [],
+        comedy: ['Up', 'Shrek', 'Finding Nemo series', 'Zootopia',
+           'The Muppet Movie', 'Despicable Me series', 'Inside Out'],
 
-        throwbacks: ['Bambi'],
+        throwbacks: ['Bambi', 'E.T. The Extra Terrestrial'],
 
         happyeverafter: {
-          princess: [],
+          princess: ['Shrek', 'Frozen'],
           nonprincess: []
         },
 
-        musicals: [],
+        musicals: ['Coco', 'Shrek', 'Frozen', 'The Muppet Movie'],
 
         fantasy: {
           space: [],
           superhero: [],
-          magic: [],
-          talkanimals: []
-        }
+          magic: ['Shrek', 'Coraline', 'Frozen', 'Moana'],
+          scifi: ['WALL-E', 'E.T. The Extra Terrestrial']
+        },
+
+        mystery: ['Coco', 'Despicable Me series'],
+
+        sad: ['Bambi', 'Up', 'Coco']
 
       }
 
@@ -99,25 +130,34 @@ movies = {
 
       genre: {
 
-        action: [],
+        action: ['Willy Wonka and The Chocolate Factory', 'Home Alone 1 & 2',
+           'The Greatest Showman', 'The Sandlot'],
 
-        comedy: [],
+        comedy: ['Willy Wonka and The Chocolate Factory', 'Cheaper by the Dozen',
+           'Matilda', 'The Parent Trap', 'Home Alone 1 & 2', 'The Sandlot'],
 
-        throwbacks: [],
+        throwbacks: ['Willy Wonka and The Chocolate Factory', 'The Sound of Music',
+          'Cheaper by the Dozen', 'The Parent Trap', 'Home Alone 1 & 2', 'The Sandlot'],
 
         happyeverafter: {
           princess: [],
-          nonprincess: ['High School Musical Series']
+          nonprincess: ['High School Musical Series', 'The Sound of Music',
+             'The Greatest Showman']
         },
 
-        musicals: ['High School Musical Series'],
+        musicals: ['High School Musical Series', 'The Sound of Music',
+           'The Greatest Showman'],
 
         fantasy: {
           space: [],
           superhero: [],
-          magic: [],
-          talkanimals: []
-        }
+          magic: ['Matilda'],
+          scifi: []
+        },
+
+        mystery: [],
+
+        sad: ['Matilda']
 
       }
 
@@ -126,3 +166,4 @@ movies = {
   }
 
 }
+
